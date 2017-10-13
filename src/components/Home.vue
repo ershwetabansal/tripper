cd9df6cebfc268fe2707769f8cdf5297f9d27e11
Shweta Bansal <ershwetabansal@energyaspects.com>
2017-10-13 22:28:03 +0100

    Remove unused methods

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,8 @@
 <template>
   <div class="home">
-    <button @click="panTo">
-      Pan To
-    </button>
-
-    <button @click="panToBounds">
-      Pan To Bounds
-    </button>
-
-    <button @click="fitBounds">
-      Fit Bounds
-    </button>
-
     <gmap-map :center="center" :zoom="7" ref="mmm" style="width: 100%; height: 500px">
-      <gmap-marker :key="index" v-for="(m, index) in markers" :position="m.position" :clickable="true" :draggable="true" @click="center=m.position"></gmap-marker>
+      <gmap-marker :key="index" v-for="(m, index) in markers" :position="m.position"
+                   :clickable="true" :draggable="true" @click="center=m.position"></gmap-marker>
     </gmap-map>
   </div>
 </template>
@@ -40,39 +29,6 @@ export default {
         }
       }]
     }
-  },
-
-  methods: {
-    fitBounds() {
-      const b = new google.maps.LatLngBounds();
-      b.extend({
-        lat: 33.972,
-        lng: 35.4054
-      });
-      b.extend({
-        lat: 33.7606,
-        lng: 35.64592
-      });
-      this.$refs.mmm.fitBounds(b);
-    },
-    panToBounds() {
-      const b = new google.maps.LatLngBounds();
-      b.extend({
-        lat: 33.972,
-        lng: 35.4054
-      });
-      b.extend({
-        lat: 33.7606,
-        lng: 35.64592
-      });
-      this.$refs.mmm.panToBounds(b);
-    },
-    panTo() {
-      this.$refs.mmm.panTo({
-        lat: 47.912867,
-        lng: 106.910723
-      });
-    },
   }
 }
 </script>
@@ -80,6 +36,6 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
-  font-weight: normal;
+  font-weight: normal
 }
 </style>
