8f7ca37abe21325e2d7d706bec9e72e078ccfece
Shweta Bansal <ershwetabansal@energyaspects.com>
2017-11-20 23:23:08 +0000

    Add a footer on the login component

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -1,23 +1,26 @@
 <template>
 <div>
   <div class="map" id="google-map"></div>
-  <ul>
-    <li @click="googleLogin" class="google">
-      <i class="fa fa-google" aria-hidden="true"></i>
-      Sign Up with Google
-    </li>
-    <li class="facebook">
-      <i class="fa fa-facebook" aria-hidden="true"></i>
-      Sign Up with Facebook
-    </li>
-    <li class="twitter">
-      <i class="fa fa-twitter" aria-hidden="true"></i>
-      Sign Up with Twitter
-    </li>
-    <li>
-      <router-link to="/home" tag="a">Continue without Sign Up</router-link>
-    </li>
-  </ul>
+  <div class="main">
+    <ul>
+      <li @click="googleLogin" class="google">
+        <i class="fa fa-google" aria-hidden="true"></i>
+        Sign Up with Google
+      </li>
+      <li class="facebook">
+        <i class="fa fa-facebook" aria-hidden="true"></i>
+        Sign Up with Facebook
+      </li>
+      <li class="twitter">
+        <i class="fa fa-twitter" aria-hidden="true"></i>
+        Sign Up with Twitter
+      </li>
+      <li>
+        <router-link to="/home" tag="a">Continue without Sign Up</router-link>
+      </li>
+    </ul>
+    <p class="footer">Sign Up is required to sync the trips/plans</p>
+  </div>
 </div>
 </template>
 
@@ -45,7 +48,7 @@
     mounted () {
       this.map = map.generate('google-map', {
         center: {lat: 0, lng: 0},
-        zoom: 1,
+        zoom: 2,
         disableDefaultUI: true
       })
     }
@@ -53,12 +56,14 @@
 </script>
 
 <style lang="scss" scoped>
-  ul {
+  .main {
     position: absolute;
     top: 0;
     margin-top: 20vh;
     padding: 10%;
     width: 100%;
+  }
+  ul {
     > li {
       padding: 10px;
       text-align: center;
@@ -101,5 +106,16 @@
     padding: 5px;
     margin-right: 5px;
     border-radius: 2px;
+  }
+
+  .footer {
+    font-size: 0.9em;
+    font-style: italic;
+    color: grey;
+    text-align: center;
+    position: fixed;
+    bottom: 30px;
+    left: 0;
+    width: 100%;
   }
 </style>
